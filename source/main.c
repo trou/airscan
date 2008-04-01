17296dbbedab3eacb1384f260c29e0ba91a6b773
Raphaël Rigo <devel@syscall.eu>
2008-04-01 22:22:16 +0200

    more params for console handling initial work on hash table

--- source/main.c
+++ source/main.c
@@ -1,16 +1,20 @@
 // Includes
 #include <PA9.h>       // Include for PA_Lib
 
-#define MAX_Y_TEXT 24
-#define MAX_X_TEXT 33
+#define MAX_Y_TEXT 24	// Number of vertical tiles
+#define MAX_X_TEXT 33	// Number of horiz tiles
+// Current console text
 char console[MAX_Y_TEXT][MAX_X_TEXT];
 int console_last = 0; /* index to the last added entry */
+int console_screen = 0, console_bg = 0;
 
-void init_console()
+void init_console(int screen, int bgnum)
 {
 	int i;
-
-	PA_InitText(1, 0);
+	
+	console_screen = screen;
+	console_bg = bgnum;
+	PA_InitText(screen, bgnum);
 	for (i = 0; i < MAX_Y_TEXT; i++)
 		console[i][MAX_X_TEXT-1] = 0;
 		
@@ -41,15 +45,48 @@ int print_to_console(char *str)
 	pos = MAX_Y_TEXT;
 	do {
 		pos--;
-		PA_OutputText(1, 0, pos, console[i]);
+		PA_OutputText(console_screen, console_bg, pos, console[i]);
 		if (--i < 0) i = MAX_Y_TEXT-1;
 	} while (pos);
 
 	return 0;
 }
 
+void abort_msg(char *msg)
+{
+	print_to_console(msg);
+	print_to_console("Exiting...");
+}
 
-// Function: main()
+struct AP_HT_Entry {
+	struct AP_HT_Entry *next;
+	Wifi_AccessPoint *ap;
+};
+
+struct AP_HT_Entry *ap_ht[256];
+
+int insert_ap(Wifi_AccessPoint *ap)
+{
+	int key	= ap->macaddr[5];
+	Wifi_AccessPoint *ap_copy;
+
+	ap_copy = (Wifi_AccessPoint *) malloc(sizeof(Wifi_AccessPoint));
+
+	if (!ap_copy)
+		abort_msg("Alloc failed !");
+
+	memcpy(ap_copy, ap, sizeof(Wifi_AccessPoint));
+
+	// check if there's already an entry in the hash table
+	if (ap_ht[key] == NULL) {
+		
+	} else {
+	}
+
+	return 0;
+}
+
+
 int main(int argc, char ** argv)
 {
 	int i;
@@ -57,7 +94,8 @@ int main(int argc, char ** argv)
 
 	PA_Init();    // Initializes PA_Lib
 	PA_InitVBL(); // Initializes a standard VBL
-	init_console();
+
+	init_console(0,0);
 
 	print_to_console("AirScan v0.1 by Raphael Rigo");
 //	print_to_console("wifi_lib_test v0.3a by Stephen Stair");
